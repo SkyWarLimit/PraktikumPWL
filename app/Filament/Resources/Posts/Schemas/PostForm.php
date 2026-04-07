0252f39092e0e91387775e92e534d9313a6bdda7
SkyWarLimit <ihsanbinrahmatw@gmail.com>
2026-04-08 00:57:46 +0700

    Jobsheet 6 02 Kelar juga Akhirnya

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -13,6 +13,9 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Schemas\Components\Section;
+use Filament\Support\Icons\Heroicon;
+use Filament\Schemas\Components\Group;
 
 class PostForm
 {
@@ -20,6 +23,14 @@ class PostForm
     {
         return $schema
             ->components([
+            Group::make([
+            // section 1 - post details
+            Section::make('Post Details')
+            ->description("Fill in the details of the post.")
+            // -> icon(Heroicon::RocketLaunch)
+            -> icon('heroicon-o-document-text')
+            ->schema([
+                Group::make([
                 TextInput::make('title')
                     ->required()
                     ->minLength(5),
@@ -31,14 +42,36 @@ class PostForm
                     ->preload()
                     ->searchable(),
                 ColorPicker::make('color'),
+                            ])->columns(2),
+
                 MarkdownEditor::make('content'),
-                // RichEditor::make('content'),
-                FileUpload::make("image")
-                    ->disk('public')
-                    ->directory('Posts'),
-                TagsInput::make('tags'),
-                Checkbox::make('published'),
-                DateTimePicker::make('published_at'),
-            ]);
+                    ]),
+
+                // section 2 - image 
+                Section::make('Image Upload')
+                ->description("Upload an image for the post.")
+                // -> icon(Heroicon::Photo)
+                -> icon('heroicon-o-photo')
+                ->schema([
+                    FileUpload::make("image")
+                        ->disk('public')
+                        ->directory('Posts'),
+                ]),
+                ])->columnSpan(2),
+
+                Group::make([
+                // section 3 - meta
+                Section::make('Meta Information')
+                ->description("Additional information about the post.")
+                // -> icon(Heroicon::InformationCircle)
+                -> icon('heroicon-o-information-circle')
+                ->schema([
+                    TagsInput::make('tags'),
+                    Checkbox::make('published'),
+                    DateTimePicker::make('published_at'),
+                    ]),
+                ])->columnSpan(1),
+            
+            ])->columns(3);
     }
 }
